eef5b9abf5ed8885d973ff0e3dc4b9cce4bf123e
mahmoud <mramadan271193@gmail.com>
2022-09-04 12:13:03 +0200

    [REM] base_agile

--- base_agile/models/project_project.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions, _
-
-
-class Project(models.Model):
-    _inherit = 'project.project'
-
-    is_scrum = fields.Boolean(string="Scrum Project OK")
